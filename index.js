906f4f1a8da8f346e8ed8cef73de4614563e65ea
Rylan Weldon <everydaynormalvlogs@gmail.com>
2024-06-21 03:30:57 -0500

    added more weather functionality & styles

--- index.js
+++ index.js
@@ -4,21 +4,41 @@ import axios from 'axios';
 
 const app = express();
 const port = 3000;
-
+let submit = false;
 const apiKey = 'a5f6f109d097624430f0629792d71c54';
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
-    res.render("index.ejs");
+    submit = false;
+    
+    res.render("index.ejs", {"submit": submit});
 })
 app.post('/submit', async (req, res)=>{
     const data = await axios.get('https://api.openweathermap.org/geo/1.0/zip?zip='+req.body.zip+',US&appid=' +apiKey);
     const parsedData = data.data;
+    const parish = parsedData.name;
+
     const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${parsedData.lat}&lon=${parsedData.lon}&appid=${apiKey}`);
     let parsedWeather = weatherData.data;
-    let temp = Math.ceil((parsedWeather.main.temp) * 9/5 -459.67);
-    res.json({"weather": temp});
+    let tempF = Math.ceil((parsedWeather.main.temp) * 9/5 -459.67); 
+    let currentWeather = parsedWeather.weather[0].main;
+    console.log(currentWeather);
+    let day = true;
+    let clarity = "clear";
+    //converts to Farienheight because for some fucking reason it has it in kelvin
+    console.log(parsedWeather.clouds.all);
+    if (parsedWeather.clouds.all > 50 && parsedWeather.clouds.all < 80){
+        clarity = "kindacloudy"
+    }
+
+    let now = new Date()
+    let hour = now.getHours();
+    if (hour > 6 && hour < 17){
+        hour = false;
+    }
+    submit = true;
+    res.render("index.ejs", {"submit": submit, "temp": tempF, "dayNight": day, "parish": parish, "current": currentWeather, "clarity": clarity})
 });
 app.listen(port, ()=>{console.log("listening on " +port);});
