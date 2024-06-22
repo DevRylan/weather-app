c2fb96a94bf52c066058bd7c050bf3fe7de44ed0
Rylan Weldon <everydaynormalvlogs@gmail.com>
2024-06-21 20:00:55 -0500

    Added majority functionality, must add @media selectors in css and clean up code.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 let submit = false;
 const apiKey = 'a5f6f109d097624430f0629792d71c54';
+let error = false;
 
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -13,34 +14,46 @@ app.use(express.static('public'));
 app.get('/', (req, res)=>{
     submit = false;
     
-    res.render("index.ejs", {"submit": submit});
+    res.render("index.ejs", {"submit": submit, "error": error});
 })
 app.post('/submit', async (req, res)=>{
+    try{
     const data = await axios.get('https://api.openweathermap.org/geo/1.0/zip?zip='+req.body.zip+',US&appid=' +apiKey);
     const parsedData = data.data;
     const parish = parsedData.name;
 
     const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${parsedData.lat}&lon=${parsedData.lon}&appid=${apiKey}`);
     let parsedWeather = weatherData.data;
-    let tempF = Math.ceil((parsedWeather.main.temp) * 9/5 -459.67); 
+    let tempFar = Math.ceil((parsedWeather.main.temp) * 9/5 -459.67); 
+    let tempFeel = Math.ceil((parsedWeather.main.feels_like) * 9/5 -459.67);
     let currentWeather = parsedWeather.weather[0].main;
+    let humidity = parsedWeather.main.humidity;
+    let windSpeed = Math.round(parsedWeather.wind.speed * 223.694 ) / 100;
     console.log(currentWeather);
     let day = true;
     let clarity = "clear";
+    let cloudCoverage = parsedWeather.clouds.all;
     //converts to Farienheight because for some fucking reason it has it in kelvin
     console.log(parsedWeather.clouds.all);
     if (currentWeather === "Clouds"){
         clarity = "kindacloudy"
     }
-
+    console.log("windspeed:" +parsedWeather.wind.speed);
     let now = new Date()
     let hour = now.getHours();
     console.log("Hour: " +hour);
-    if (hour > 6 && hour < 19){
+    if (hour > 6 && hour < 20){
         day = false;
     }
     console.log("Day is " +day);
     submit = true;
-    res.render("index.ejs", {"submit": submit, "temp": tempF, "dayNight": day, "parish": parish, "current": currentWeather, "clarity": clarity})
+    error = false;
+    res.render("index.ejs", {"submit": submit, "temp": tempFar, "dayNight": day, "parish": parish, "current": currentWeather, "clarity": clarity, "error": error, "feel": tempFeel, "humidity": humidity, "speed": windSpeed, "coverage": cloudCoverage})
+}
+catch (error){
+    error = true;
+    let response = req.body.zip;
+    res.render("index.ejs", {"submit": submit, "error": error, "response": response});
+}
 });
 app.listen(port, ()=>{console.log("listening on " +port);});
